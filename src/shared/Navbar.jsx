0262a6b973634abea9092ac6f7db9d65fe5fb453
asad2002web <asad2002web@gmail.com>
2023-12-09 18:25:30 +0600

    test-case[1]

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Smile &copy; </a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
